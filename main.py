4a79a4554e06e8c6227f925b9b991a52811a146f
PureJuly <mymin8724@naver.com>
2026-02-10 09:52:41 +0900

    계좌 개설 기능 추가

---
+++ main.py
@@ -0,0 +1,45 @@
+class Account:
+    def __init__(self, acc_id, name, balance)
+        self.acc_id = acc_id
+        self.balance = balance
+        self.name = name
+
+    def get_acc_id(self):
+        return self.acc_id
+
+    def deposit(self, money):
+        self.balance += money
+
+    def withdraw(self. money):
+        if self.balance < money:
+            return 0
+        self.balance -= money
+        return money
+
+    def show_info(self):
+        print(f"계좌ID: {self.acc_id}")
+        print(f"이름: {self.name}")
+        print(f"잔액: {self.balance}\n")
+
+acc_arr = []
+
+def show_menu():
+    print("-----Menu-----")
+    print("1. 계좌개설")
+    print("2. 입금")
+    print("3. 출금")
+    print("4. 계좌번호 전체 출력")
+    print("5. 프로그램 종료")
+
+def make_account():
+    print("[계좌개설]")
+    try:
+        acc_id = make_account(input("계좌ID: (숫자로 입력) "))
+        name = input("이름: ")
+        balance = int(input("입금액: "))
+        print()
+    except valueError
+        print("\n입력 형식이 올바르지 않습니다.\n")
+        return
+
+acc_arr.append(Account(acc_id, balance=, name))
